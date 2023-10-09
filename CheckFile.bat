a5bab80a40b4fe233b09f6c93d3334cd79a79c5b
Berente Péter <40863181+berentepeter@users.noreply.github.com>
2023-10-09 15:29:23 +0200

    Add files via upload

---
+++ CheckFile.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+PowerShell.exe -Command "C:\...\CheckFile.ps1"
+pause
---
+++ CheckFile.ps1
@@ -0,0 +1,21 @@
+﻿$file = Get-Item -Path \\filepath...
+
+$last_modified = '12 September ‎2022 ‏‎03:43:17'
+
+$time = $file.lastwritetime.DateTime
+
+if($time -eq $last_modified){
+    write-host "OK            OK             OK             10590 OK" -ForegroundColor green 
+}else{
+    Add-Type -AssemblyName System.speech
+    $speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
+    $speak.SelectVoice('Microsoft Zira Desktop')
+    $speak.GetInstalledVoices().VoiceInfo
+    [system.media.systemsounds]::Exclamation.play()
+    $speak.Speak("Error")
+    $speak.Dispose()
+    write-host "Error" -ForegroundColor red
+    write-host "File compromised!" -ForegroundColor red
+}
+
+pause
